9eb7ff755af494d0dfa78e823dc2872f6f33cf60
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-08-25 16:34:32 +0100

    Breadcrumbs method

--- TimeComplexity/index.js
+++ TimeComplexity/index.js
@@ -1,31 +1,39 @@
-let isUnique = (arr) => {
+// let isUnique = (arr) => {
+//   let result = true;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`outer loop--- i === ${i}`);
+//     for (let j = 0; j < arr.length; j++) {
+//       console.log(`inner loop--- i === ${j}`);
+//       if (i !== j && arr[i] === arr[j]) {
+//         result = false;
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+// console.log(isUnique([1, 2, 3]) === true);
+// console.log(isUnique([1, 2, 3]) === false);
+
+// the breadcrumb method
+
+let isUnique= (arr) => {
+  const breadCrumbs = {};
   let result = true;
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`outer loop--- i === ${i}`);
-    for (let j = 0; j < arr.length; j++) {
-        console.log( `inner loop--- i === ${ j }` );
-        if (i !== j && arr[i] === arr[j] ) {
-            result = false
-        }
+    console.log(`loop i === ${i}`);
+    if (breadCrumbs[arr[i]]) {
+          result = false;
+          console.log(`if - ${breadCrumbs[arr[i]]}`);
+    } else {
+      breadCrumbs[arr[i]] = true;
+      console.log( ` else - ${breadCrumbs[arr[i]]}`);
     }
   }
-  return result;
+      return result;
 };
 
 console.log(isUnique([1, 2, 3]) === true);
-console.log(isUnique([1, 2, 3]) === false);
-
-
-isUnique = ( arr ) =>
-{
-    const breadCrumbs = {}
-    let result = true
-
-    for (let i  = 0; i  < array.length; i ++) {
-       console.log(`loop i === ${i}`);
-       if (breadCrumbs[arr[1]]) {
-        
-       } 
-    }
-}
+// console.log(isUnique([1, 2, 3]) === false);
