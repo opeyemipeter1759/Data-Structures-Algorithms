e0815f0bfca16ed25ccded4395ecffdb54fbe822
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2022-08-28 21:39:40 +0100

    Caching with memoization

--- TimeComplexity/index.js
+++ TimeComplexity/index.js
@@ -18,55 +18,68 @@
 
 // the breadcrumb method
 
-let isUnique= (arr) => {
+let isUnique = (arr) => {
   const breadCrumbs = {};
   let result = true;
 
   for (let i = 0; i < arr.length; i++) {
     console.log(`loop i === ${i}`);
     if (breadCrumbs[arr[i]]) {
-          result = false;
-          console.log(`if - ${breadCrumbs[arr[i]]}`);
+      result = false;
+      console.log(`if - ${breadCrumbs[arr[i]]}`);
     } else {
       breadCrumbs[arr[i]] = true;
-      console.log( ` else - ${breadCrumbs[arr[i]]}`);
+      console.log(` else - ${breadCrumbs[arr[i]]}`);
     }
   }
-      return result;
+  return result;
 };
 
 // console.log(isUnique([1, 2, 3]) === true);
 // console.log(isUnique([1, 2, 3]) === false);
 
-
 const items = [
-      { name: 'Edward', value: 21 },
-      { name: 'Sharpe', value: 37 },
-      { name: 'And', value: 45 },
-      { name: 'The', value: -12 },
-      { name: 'Magnetic', value: 13 },
-      { name: 'Zeros', value: 37 }
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 },
 ];
-    
-items.sort( ( a, b ) => a.value - b.value )
 
-let newsArr = [1, 70, 49, 54, 5, 6]
-newsArr.sort()
-function    compareNumbers(a, b) {
-      return a - b;
-    }
+items.sort((a, b) => a.value - b.value);
 
-// console.log( newsArr ); 
-let newest = newsArr.sort( ( a, b ) => a - b )
+let newsArr = [1, 70, 49, 54, 5, 6];
+newsArr.sort();
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+// console.log( newsArr );
+let newest = newsArr.sort((a, b) => a - b);
 //    console.log(`newest -- ${newest}`);
 // console.log( newsArr.sort( compareNumbers ) )
 
-
 const uniqSort = function (arr) {
-      const breadCrumb = {}
-  
-      return [...new Set(arr.sort( ( a, b ) => a - b ))]
+  const breadCrumb = {};
+
+  return [...new Set(arr.sort((a, b) => a - b))];
+};
+const uniqSort2 = function (arr) {
+  const breadCrumbsSort = {};
+  const resultSort = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    // element 0 can never be a duplicate
+    if (!breadCrumbsSort[arr[i]]) {
+      resultSort.push(arr[i]);
+      breadCrumbsSort[arr[i]] = true;
+      console.log(breadCrumbsSort[arr[i]]);
 }
+  }
+  return resultSort.sort((a, b) => a - b);
+};
 
-console.log(uniqSort( [4, 2, 2, 3, 2, 2, 2] ));
+// console.log(uniqSort([4, 2, 2, 3, 2, 2, 2]));
 
+
+console.log(`sort 2 ---${uniqSort2([4, 2, 2, 3, 2, 2, 2])}`);
---
+++ TimeComplexity/memoize.js
@@ -0,0 +1,33 @@
+// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
+// a simple multiplication fn
+const times10 = (n) => n * 10;
+
+console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
+// console.log('times10 returns:', times10(9));
+
+// Task 2: Use an object to cache the results of your times10 function.
+// protip 1: Create a function that checks if the value for n has been calculated before.
+// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+
+const cache = { };
+
+const memoTimes10 = (n) => {
+  if (n in cache) {
+    console.log(`fetching from cache ${n}`);
+    return cache[n];
+  } else {
+      console.log( 'calculating the result' );
+    let result = times10(n);
+    cache[n] = result;
+    return result;
+  }
+};
+
+// console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
+console.log( 'Task 2 calculated value:', memoTimes10( 9 ) ) // calculated
+console.log(cache);
+console.log( 'Task 2 cached value:', memoTimes10( 9 ) ) // cached
+console.log( 'Task 2 calculated value:', memoTimes10( 70 ) ) // calculated
+
+console.log(cache);
+
